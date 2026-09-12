8caa390c3a9f26977fe4190fff0d3b6ce9d5cc79
iggyuga <iggyuga@users.noreply.github.com>
2026-09-12 17:46:07 +0000

    deploy: a89c72f05723776d0b675ec596d73e296c5fb783

--- assets/arrow-left-DcED43vZ.js
+++ assets/arrow-left-DcED43vZ.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-COMSVEdS.js";/**
+import{c as e}from"./index-Cp9kqEw3.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
